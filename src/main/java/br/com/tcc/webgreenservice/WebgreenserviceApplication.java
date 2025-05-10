9eefe43dde103d8deb45aae2e81cc8d05fc1c9a7
ArthurTorres11 <arthur_torres_camargo@hotmail.com>
2025-05-09 23:13:00 -0300

    Iniciando o projeto

---
+++ src/main/java/br/com/tcc/webgreenservice/WebgreenserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcc.webgreenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgreenserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgreenserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tcc/webgreenservice/WebgreenserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcc.webgreenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgreenserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
